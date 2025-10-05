cdc6829c7399e89b48d52709d7d69dae54b364ef
NgetSokunkanha <ngetsokunkanha1@gmail.com>
2025-10-05 22:21:23 +0700

    ex1 done

--- ex1_w0.cpp
+++ ex1_w0.cpp
@@ -1,9 +1,31 @@
 
 #include <iostream>
+#include <array>
 using namespace std;
 
+void insertElement(int n , array<int , 5 > numbers ) {
+    if ( n<5 && n>=0 ) {
+        int valueNumber;
+        cout << "Enter the value: ";
+        cin >> valueNumber;
+        numbers[n] = valueNumber;
+        for ( int i =0; i<5;i++) {
+            cout << numbers[i];
+        } 
+    } else {
+        cout << "There is no " << n << " element.Please enter again!";
+    }
+
+
+
+}
+
 int main () {
-    cout << "Hello";
+    array<int, 5> numbers = {5,4,3,2,1};
+    int n;
+    cout << "Enter the element you want to insert: ";
+    cin >> n;
+    insertElement(n,numbers);
     return 0;
 }
 
